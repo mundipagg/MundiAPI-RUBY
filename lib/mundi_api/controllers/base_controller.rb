f514aa879bbfb2c317846901bae4d33337164a79
Marcelo Marini <marcelomarini17@hotmail.com>
2020-01-21 20:39:31 +0000

    :wrench: Fix ListCyclesResponse data type

--- lib/mundi_api/controllers/base_controller.rb
+++ lib/mundi_api/controllers/base_controller.rb
@@ -13,7 +13,7 @@ module MundiApi
       @http_call_back = http_call_back
 
       @global_headers = {
-        'user-agent' => 'MundiSDK - Ruby 0.16.8'
+        'user-agent' => 'MundiSDK - Ruby 0.16.9'
       }
     end
 
